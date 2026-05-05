e230823b2eb8c2cbe13e7b04c66a72161f724d3f
Gustavo S. Marmo <gsmarmo@gmail.com>
2026-05-05 20:02:12 -0300

    initial commit

---
+++ src/test/java/com/firstech/FirstechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
