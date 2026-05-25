3c276a35833f69c631f146d96b8e7c7041b7e778
gustavomarmo <gsmarmo@gmail.com>
2026-05-24 21:10:10 -0300

    feat: add tailwindConfig.js

---
+++ src/main/resources/static/js/tailwindConfig.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        bg:      '#0d1117',
+        surface: '#151b24',
+        purple:  '#8b5cf6',
+        'purple-light': '#a78bfa',
+        'bg-light': '#ffffff',
+        'surface-light': '#f3f4f6'
+      }
+    }
+  }
+}
